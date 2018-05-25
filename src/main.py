11b8cb23ca7f1520de2d3a73989c573caf5c931e
Chris Chen <9254734@qq.com>
2018-05-25 23:48:36 +0100

    Cali

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src import black_sholes, heston,reader
 from scipy.optimize import fmin
-
+import pandas as pd
 
 #sample market data
 def sample_data():
@@ -36,24 +36,30 @@ def calibrate(init_val, market_datas):
 if __name__ == '__main__':
     #load market data
     header, market_datas = sample_data()
-    market_datas = reader.getArrays()
+
+    for yearNumber in ["2008","2009","2010"]:
+
+        market_datas = reader.getArrays(yearNumber)
     #Initialize kappa, theta, sigma, rho, v0
-    init_val = [1.1, 0.1, 0.4, -0.6, 0.1]
-    #calibration of parameters
-    test = calibrate(init_val, market_datas)
-    [kappa, theta, sigma, rho, v0] = calibrate(init_val, market_datas)
-    print ("kappa:{0}, theta:{1}, sigma:{2}, rho:{3}, v0:{4}".format(kappa, theta, sigma, rho, v0))
+        init_val = [1.1, 0.1, 0.4, -0.6, 0.1]
+        #calibration of parameters
+        test = calibrate(init_val, market_datas)
+        [kappa, theta, sigma, rho, v0] = calibrate(init_val, market_datas)
+        print ("kappa:{0}, theta:{1}, sigma:{2}, rho:{3}, v0:{4}".format(kappa, theta, sigma, rho, v0))
+        result = pd.DataFrame([kappa, theta, sigma, rho, v0])
+        result.to_csv("cal"+yearNumber+".csv")
+
     #
-    market_prices = np.array([])
-    heston_prices = np.array([])
-    K = np.array([])
-    for market_data in market_datas:
-        s0, k, market_price, r, T = market_data
-        heston_prices = np.append(heston_prices, heston.call_price(kappa, theta, sigma, rho, v0, r, T, s0, k))
-        market_prices = np.append(market_prices, market_price)
-        K = np.append(K,k)
-    #plot result
-    plt.plot(K, market_prices, 'g*',K, heston_prices, 'b')
-    plt.xlabel('Strike (K)')
-    plt.ylabel('Price')
-    plt.show()
+    # market_prices = np.array([])
+    # heston_prices = np.array([])
+    # K = np.array([])
+    # for market_data in market_datas:
+    #     s0, k, market_price, r, T = market_data
+    #     heston_prices = np.append(heston_prices, heston.call_price(kappa, theta, sigma, rho, v0, r, T, s0, k))
+    #     market_prices = np.append(market_prices, market_price)
+    #     K = np.append(K,k)
+    # #plot result
+    # plt.plot(K, market_prices, 'g*',K, heston_prices, 'b')
+    # plt.xlabel('Strike (K)')
+    # plt.ylabel('Price')
+    # plt.show()
--- src/reader.py
+++ src/reader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def getArrays ():
-    option2008 = pd.read_csv('result/2017.csv');
+def getArrays (year):
+    option2008 = pd.read_csv('result/'+year+'.csv');
 
     result = pd.DataFrame({'S':option2008["close"], 'K':option2008["strike_price"], 'price':option2008["price"], 'rf':option2008["rf"],'dtm':option2008["day_diff"]}, columns=['S', 'K', 'price', 'rf', 'dtm'])
     result = result.reset_index(drop=True)
@@ -11,4 +11,4 @@ def getArrays ():
     print(len(arrays))
     return arrays
 
-getArrays()
+# getArrays()
