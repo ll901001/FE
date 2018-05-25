4dff313efae6115d9dbd978d443ffd45ce4dae4d
Chris Chen <9254734@qq.com>
2018-05-25 19:31:24 +0100

    muti2

--- src/reader.py
+++ src/reader.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
 def getArrays ():
-    option2008 = pd.read_csv('result/result2017.csv');
-
-    option2008 = option2008[(option2008["S/K"]<1.2) & (option2008["S/K"]> 0.8)]
-    option2008 = option2008[option2008["day_diff"]==180]
+    option2008 = pd.read_csv('result/2017.csv');
 
     result = pd.DataFrame({'S':option2008["close"], 'K':option2008["strike_price"], 'price':option2008["price"], 'rf':option2008["rf"],'dtm':option2008["day_diff"]}, columns=['S', 'K', 'price', 'rf', 'dtm'])
     result = result.reset_index(drop=True)
     result["dtm"]= result['dtm'].divide(360)
     arrays = result.as_matrix()
 
-    print(arrays)
+    print(len(arrays))
     return arrays
 
 getArrays()
