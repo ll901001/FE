a86d06a0c59258bc7c02dd3b7359e9387613b900
Chris Chen <9254734@qq.com>
2018-05-27 10:01:06 +0100

    error fun

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ def sample_data():
 #parameter calibration(kappa, theta, sigma, rho, v0)
 def calibrate(init_val, market_datas):
 
-    opt = minimize(error, init_val, args = (market_datas,), bounds = ((1.01,5),(0.01,1),(0.01, 1),(-0.88,-0.45),(0.01,1)), method='L-BFGS-B',options = {'maxiter': 20})
+    opt = minimize(error, init_val, args = (market_datas,), bounds = ((1.01,5),(0.01,1),(0.01, 1),(-0.88,-0.45),(0.01,1)), method='Nelder-Mead',options = {'maxiter': None})
     # opt = fmin(error, init_val, args=(market_datas,),maxiter= 20)
     return opt
 
@@ -28,35 +28,33 @@ def error(x, market_datas):
         # print ("s0:{0}, k:{1}, market_price:{2}, r:{3}, T:{4}".format(s0, k, market_price, r, T))
 
         heston_price = heston.call_price(kappa, theta, sigma, rho, v0, r, T, s0, k)
-        # if(heston_price - market_price)**2/market_price**2 > 0.5:
-        #     result += 100
-        # elif (heston_price - market_price) ** 2 / market_price ** 2 > 0.01:
-        #     print("heston_price:{0}, market_price:{1}, acc:{2}".format(heston_price, market_price, 1))
-        #     result += 10
-        # elif (heston_price - market_price) ** 2 / market_price ** 2 > 0.0001:
-        #     print("heston_price:{0}, market_price:{1}, acc:{2}".format(heston_price, market_price, 0.01))
-        #     result += 1
-        # else:
-        #     print("heston_price:{0}, market_price:{1}, acc:{2}".format(heston_price, market_price, 0.0001))
-        result += (heston_price - market_price)**2/market_price**2/vega**2
+
+        error= (heston_price - market_price)**2/market_price**2/vega**2
+        result += error
+
+    if (kappa < 1.01) | (kappa > 5):
+        result+= error * 10
+    if (theta < 0.01) | (theta > 1):
+        result+= error * 10
+    if (sigma < 0.01) | (sigma > 1):
+        result+= error * 10
+    if (rho < -0.88) | (rho > -0.45):
+        result+= error * 10
+    if (v0 < 0.01) | (v0 > 1):
+        result+= error * 100
     return result
 
 if __name__ == '__main__':
     #load market data
     header, market_datas = sample_data()
 
-    for yearNumber in ["2012","2013","2014","2015","2016","2017"]:
+    for yearNumber in ["2012"]:
 
         market_datas = reader.getArrays(yearNumber)
     #Initialize kappa, theta, sigma, rho, v0
-        # init_val = [1.1, 0.1, 0.4, -0.6, 0.1]
-        init_val = [1.7857335413857758, 0.09828053359611841, 0.7616109388424428, -0.8383242759610362, 0.4]
-        #calibration of parameters
-        # [kappa, theta, sigma, rho, v0] = calibrate(init_val, market_datas).x
-        # print ("kappa:{0}, theta:{1}, sigma:{2}, rho:{3}, v0:{4}".format(kappa, theta, sigma, rho, v0))
-        # result = pd.DataFrame([kappa, theta, sigma, rho, v0] )
-        # while True:
-        #     init_val[4] = 0.1
+        init_val = [1.1, 0.1, 0.4, -0.6, 0.1]
+        # init_val = [1.7857335413857758, 0.09828053359611841, 0.76161049388424428, -0.8383242759610362, 0.1]
+
         test = calibrate(init_val, market_datas)
         #     init_val = test.x
         #
